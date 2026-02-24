74b48c978d4e49f1cfc5bc453792baf71e11fea1
Ysmar <37018777+ysmarts@users.noreply.github.com>
2026-02-24 19:07:09 +0000

    modified the phrase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Victoria, eu te amo. Se você me ama, dá uma risadinha rs</h1>
+    return <h1>Victoria, eu te amo! Se você me ama, dá uma risadinha rsrs</h1>
 }
 
 export default Home;
